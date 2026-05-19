289527d6745a83d927c551906f6ba485b183fa06
Atul <atul.srsit@gmail.com>
2026-05-19 22:58:46 +0530

    Add var, let, and TDZ explained examples

---
+++ Basics_06_Var_explained.js
@@ -0,0 +1,13 @@
+var v=10;
+console.log(v);
+
+function test(){
+    var v=20;
+    console.log(v);
+}
+test(); 
+
+
+console.log(v);
+var v=10;
+console.log(v);
---
+++ Basics_07_let_explained.js
@@ -0,0 +1,11 @@
+let x=10;
+console.log(x);
+
+function test(){
+    let x=20;
+    console.log(x);
+}
+
+test();
+/*let x=30;
+console.log(x);*/
