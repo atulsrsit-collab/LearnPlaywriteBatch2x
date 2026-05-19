50ca0ad89fb74d928e3776e6e491f76729f5dd29
Atul <atul.srsit@gmail.com>
2026-05-20 00:16:34 +0530

    Add Literal_09_all.js with all JavaScript number types

---
+++ Literal_09_all.js
@@ -0,0 +1,34 @@
+// Integer (decimal)
+console.log("Integer:", 42);
+
+// Floating point
+console.log("Float:", 3.14);
+
+// Scientific notation
+console.log("Scientific:", 1e3);
+console.log("Scientific:", 1.5e-2);
+
+// Binary
+console.log("Binary 0b1010:", 0b1010);
+
+// Octal
+console.log("Octal 0o12:", 0o12);
+
+// Hexadecimal
+console.log("Hex 0xFF:", 0xFF);
+
+// BigInt
+console.log("BigInt:", 123n);
+
+// Negative
+console.log("Negative:", -10);
+
+// Infinity
+console.log("Infinity:", Infinity);
+console.log("Negative Infinity:", -Infinity);
+
+// NaN
+console.log("NaN:", NaN);
+
+// Numeric separator (ES2021)
+console.log("Separator 1_000_000:", 1_000_000);
