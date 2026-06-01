ebc30cee0df0a04586409692f499431c5bf86ca8
Atul <atul.srsit@gmail.com>
2026-06-01 19:08:33 +0530

    Add interview prep switch example

---
+++ interviewprepswitch.js
@@ -0,0 +1,22 @@
+let fruit = "carrot";
+switch(fruit)
+{
+    case "banana":
+        {
+            console.log("this is banana fruit");
+            break;
+        }
+    case "apple":
+        {
+            console.log("this is apple fruit");
+        break;
+        }
+    case "jackfruit":
+        {
+            console.log("this is jackfruit");
+        break;
+        }
+    default:
+              console.log("no fruit to eat");
+        
+    }
