fce32d2da9952c7d452deec78c6255443a83b356
Atul <atul.srsit@gmail.com>
2026-06-12 00:53:08 +0530

    Initial commit

---
+++ ArraySplice.js
@@ -0,0 +1,3 @@
+let marvelheros=["atul","kumar","singh","teena","thakur"];
+console.log(marvelheros);
+console.log(marvelheros.splice(2,2,"maanya","singh"));
---
+++ Arrayslice.js
@@ -0,0 +1,3 @@
+let marvelheros=["ironman","hulk","spidy","superman","shakti"];
+console.log(marvelheros);
+console.log(marvelheros.slice(1,8));
