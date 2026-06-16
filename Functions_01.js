faa4f2517dc7c7fdc02bc5daa0da11c9f009266c
Atul <atul.srsit@gmail.com>
2026-06-16 23:00:08 +0530

    Initial commit: JavaScript function practice files

---
+++ Functions_01.js
@@ -0,0 +1,25 @@
+// function sum(a,b)
+// {
+//     let x= a+b;
+//     console.log(x);
+
+// }
+
+//Below program is to print the addition using arrow function
+
+/*const arrowsum=(a,b) =>
+{
+    console.log(a+b);
+}
+
+arrowsum(2,3);
+console.log(arrowsum);
+*/
+//Below program is to run the multiplication program
+
+const arrowfunc1=(c,d)=>
+{
+    console.log(c*d);}
+
+arrowfunc1(5*10);
+console.log(arrowfunc1);
---
+++ Functions_02.js
@@ -0,0 +1,21 @@
+function vowelcount(str)
+{
+ let count=0;
+ for (const char of str)
+ {
+ if(char==="a"|| 
+    char==="e"|| 
+    char ==="i"|| 
+    char ==="o"|| 
+    char==="u")
+
+    {
+        count++;
+    }
+ }
+
+ return count;
+}
+
+vowelcount("atuul");
+console.log(vowelcount("atul"));
